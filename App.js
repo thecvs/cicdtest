fc6c330299b94b15915d7eabf35d6ef19d217eec
Chandravardhan singh Solanki <chandravardhansingh1@gmail.com>
2022-09-19 21:36:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Greet from './src/component/Greet';
+
+const App = () => {
+  return <Greet />;
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/component/Greet.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Greet = () => {
+  return (
+    <View style={styles.greet}>
+      <Text style={styles.text}>Hello welcome to CI/CD Course</Text>
+    </View>
+  );
+};
+
+export default Greet;
+
+const styles = StyleSheet.create({
+  greet: {
+    width: '90%',
+    alignSelf: 'center',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {color: '#000', fontSize: 20, fontWeight: '700'},
+});
